ad16237dcd21f018ebbc9597b87efe03d75b9dbb
OmAn94 <jaredomar@gmail.com>
2026-05-30 23:20:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-{
+module.exports = {
   "name": "MangaFire.to (0.8)",
-  "description": "Fuente personalizada para mangafire.to – compatible con Paperback 0.8",
-  "icon": "https://mangafire.to/favicon.ico",
-  "website": "https://mangafire.to",
   "sources": [
     {
       "id": "mangafire",
